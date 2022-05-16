ea1bcaca020e64c6c3c9f1cc41ed6775e4aaadde
anagilvieira <74660365+anagilvieira@users.noreply.github.com>
2022-05-16 14:29:02 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import MODELS
-from .siamese import SiameseNetwork
-from .resnet2 import ResNet
-from .simresnet import SimResNet
--- models/utils.py
+++
@@ -1,5 +0,0 @@
-MODELS = {}
-
-def register_model(cls):
-    MODELS[cls.__name__] = cls
-    return cls
